be75a814ba1f1713bbbd7b6b96e1e0950127d60b
Pankaj Kumar <pankk217@in.ibm.com>
2024-11-04 18:31:22 +0530

    :link: Added link class for styling

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-import Image from 'next/image';
+
 const Header = () => {
   return (
     <header className='flex flex-col justify-center bg-primary sticky top-0 z-10 text-primary-foreground h-16 px-8'>
-      <Link href='/'>
+      <Link href='/' className='w-fit'>
         <h1 className='scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 font-petit'>Lucaread</h1>
       </Link>
     </header>
